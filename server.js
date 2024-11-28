be59ad018ccf82881e457de6bd9c487041910c83
Aswinja05 <asja22ise@cmrit.ac.in>
2024-11-28 19:38:31 +0530

    Server updated with mongodb

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 const app = express();
+
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
+
+
+
+//DATABASE
+mongoose.connect('mongodb+srv://admin:Admin123@donortrack.fr6p2.mongodb.net/DonorTrackDB?retryWrites=true&w=majority'
+, {})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 
 
@@ -11,10 +28,20 @@ app.get('/',(req,res)=>{
 
 
 
-//Hi from akash
 
 
+//Calling api
 
+// const accountSid = "AC122f8454ba8f1b48cc924acfd3e1e133";
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = require("twilio")(accountSid, authToken);
+
+// client.calls.create({
+//   url: "http://demo.twilio.com/docs/voice.xml",
+//   to: "+919902227821",
+//   from: "+17753699722",
+// })
+// .then(call => console.log(call.sid));
 
 
 
